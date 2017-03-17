4078e64f3a712bac3ce80f5f1c7cdb037d4ad67a
Mystique <b4wt@quantumfoam.uni.cc>
2017-03-17 16:26:59 +0530

    Signed-off-by: Mystique <b4wt@quantumfoam.uni.cc>

--- How-To/setup-s3-block-storage-filesystem/setup-s3-block-storage-filesystem.py
+++ How-To/setup-s3-block-storage-filesystem/setup-s3-block-storage-filesystem.py
@@ -1,46 +1,32 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
-# Using NAT Instances to connect Private instance to internet
-
+### Set AWS Resources
+# The AWS Resources options are set globally in python dict. This allows the user to customize the script to their region(for example change the region_name & AMI ID to your needs etc.,)
 import boto3
 
-
-# Check if the user has the Access & Secret key configured
-from boto3 import Session
-session = Session()
-credentials = session.get_credentials()
-current_credentials = credentials.get_frozen_credentials()
-
-# Break & Exit if any of the key is not present
-if current_credentials.access_key is None:
-    print "Access Key missing, use  `aws configure` to setup"
-    exit()
-
-if current_credentials.secret_key is None:
-    print "Secret Key missing, use  `aws configure` to setup"
-    exit()
-
-globalVars  = {}
+globalVars = {}
 globalVars['REGION_NAME']           = "ap-south-1"
 globalVars['AZ1']                   = "ap-south-1a"
 globalVars['AZ2']                   = "ap-south-1b"
-globalVars['CIDRange']              = "10.242.0.0/24"
-globalVars['tagName']               = "miztiik-wp-demo-00"
+globalVars['CIDRange']              = "10.243.0.0/24"
+globalVars['tagName']               = "miztiik-s3fs-demo-04"
 globalVars['EC2-AMI-ID']            = "ami-cdbdd7a2"
 globalVars['EC2-InstanceType']      = "t2.micro"
-globalVars['EC2-KeyName']           = "wp-key"
+globalVars['EC2-KeyName']           = "s3fs-key"
+globalVars['S3-BucketName']         = "miztiik-demo-s3fs-bucket"
 
+### Setup Networks
+# The VPC, Subnet, Internet Gateway, Routing Table & Security Groups are created as [described here](https://github.com/miztiik/AWS-Demos/tree/master/How-To/setup-multi-az-vpc-from-scratch-using-boto)
 # Creating a VPC, Subnet, and Gateway
 ec2         = boto3.resource ( 'ec2', region_name = globalVars['REGION_NAME'] )
 ec2Client   = boto3.client   ( 'ec2', region_name = globalVars['REGION_NAME'] )
-vpc         = ec2.create_vpc ( CidrBlock = globalVars['CIDRange']  )
+s3Client    = boto3.client   ( 's3',  region_name = globalVars['REGION_NAME'] )
 
-
+vpc         = ec2.create_vpc ( CidrBlock = globalVars['CIDRange'] )
 # AZ1 Subnets
-az1_pvtsubnet   = vpc.create_subnet( CidrBlock = '10.242.0.0/25'   , AvailabilityZone = globalVars['AZ1'] )
-az1_pubsubnet   = vpc.create_subnet( CidrBlock = '10.242.0.128/26' , AvailabilityZone = globalVars['AZ1'] )
-az1_sparesubnet = vpc.create_subnet( CidrBlock = '10.242.0.192/26' , AvailabilityZone = globalVars['AZ1'] )
+az1_pvtsubnet   = vpc.create_subnet( CidrBlock = '10.243.0.0/25'   , AvailabilityZone = globalVars['AZ1'] )
+az1_pubsubnet   = vpc.create_subnet( CidrBlock = '10.243.0.128/26' , AvailabilityZone = globalVars['AZ1'] )
+az1_sparesubnet = vpc.create_subnet( CidrBlock = '10.243.0.192/26' , AvailabilityZone = globalVars['AZ1'] )
 
 
 # Enable DNS Hostnames in the VPC
@@ -116,49 +102,54 @@ if not next((key for key in customEC2Keys if key["KeyName"] == globalVars['EC2-K
     print ("New Private Key created,Save the below key-material\n\n")
     print ( ec2_key_pair.key_material )
 
+### Create S3 Bucket
+
+# **No Validations** are being done to check for pre-existing bucket s
+    # The bucket does not exist or you have no access.
+s3Bucket = s3Client.create_bucket( ACL='private', 
+                                   Bucket = globalVars['S3-BucketName'], 
+                                   CreateBucketConfiguration = { 'LocationConstraint': globalVars['REGION_NAME'] }
+                                )
+
+
+
+
+
+
+# Create the S3-FS host Instance
+
 # Using the userdata field, we will download, install & configure our basic word press website.
 # The user defined code to install Wordpress, WebServer & Configure them
 userDataCode = """
 #!/bin/bash
 set -e -x
 
-# Setting up the HTTP server 
-yum install -y httpd php php-mysql mysql
-systemctl start httpd
-systemctl enable httpd
-groupadd www
-usermod -a -G www ec2-user
+# Install FUSE Packages
+# Ref - https://github.com/s3fs-fuse/s3fs-fuse
+yum -y install automake fuse fuse-devel gcc-c++ git libcurl-devel libxml2-devel make openssl-devel
+git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+cd s3fs-fuse
+./autogen.sh
+./configure
+make
+make install
 
 
-# Download wordpress site & move to http
-cd /var/www/
-curl -O https://wordpress.org/latest.tar.gz && tar -zxf latest.tar.gz
-rm -rf /var/www/html
-mv wordpress /var/www/html
-
-# Set the permissions
-chown -R root:www /var/www
-chmod 2775 /var/www
-find /var/www -type d -exec chmod 2775 {} +
-find /var/www -type f -exec chmod 0664 {} +
-
-# SE Linux permissive
-# needed to make wp connect to DB over newtork
-setsebool -P httpd_can_network_connect=1
-setsebool httpd_can_network_connect_db on
-
-systemctl restart httpd
-# Remove below file after testing
-echo "<?php phpinfo(); ?>" > /var/www/html/phptestinfo.php
+# Enter the Identity(Access-Key)&Credentials(Secret-Key)
+echo "<A.C.C.E.S.S-K.E.Y>:<A.C.T.U.A.L-S.E.C.R.E.T-K.E.Y>" > /root/.s3fs-passwd
+chmod 600 /root/.s3fs-passwd
+# Mount s3fs with an existing bucket
+mkdir -p /var/s3fs-demo-fs
+s3fs miztiik-demo-s3fs-bucket /var/s3fs-demo-fs -o passwd_file=/root/.s3fs-passwd
+echo "This is a test text input" >>/var/s3fs-demo-fs/test-file-during-demo.txt
 """
 
-# Create the  Public Word Press Instance
+
 ##### **DeviceIndex**:The network interface's position in the attachment order. For example, the first attached network interface has a DeviceIndex of 0 
 instanceLst = ec2.create_instances(ImageId = globalVars['EC2-AMI-ID'],
                                    MinCount=1,
                                    MaxCount=1,
                                    KeyName=globalVars['EC2-KeyName'] ,
-                                   UserData = userDataCode,
                                    InstanceType = globalVars['EC2-InstanceType'],
                                    NetworkInterfaces=[
                                                         {
@@ -170,6 +161,14 @@ instanceLst = ec2.create_instances(ImageId = globalVars['EC2-AMI-ID'],
                                                         }
                                                     ]
                                 )
+
+#### Sample Output
+[root@ip-10-243-0-147 ~]# df -h /var/s3fs-demo-fs
+Filesystem      Size  Used Avail Use% Mounted on
+s3fs            256T     0  256T   0% /var/s3fs-demo-fs
+
+[root@ip-10-243-0-147 ~]# more /var/s3fs-demo-fs/test-file-during-demo.txt
+This is a test text input
 
 """
 Function to clean up all the resources
@@ -209,4 +208,11 @@ def cleanAll(resourcesDict=None):
     pubSecGrp.delete()
     pvtSecGrp.delete()
 
+    # Delete VPC
     vpc.delete()
+
+    # Delete S3 Bucket
+    s3 = boto3.resource( 's3',  region_name = globalVars['REGION_NAME'] )
+    for key in bucket.objects.all():
+        key.delete()
+    s3Client.delete_bucket( Bucket = globalVars['S3-BucketName'] )
