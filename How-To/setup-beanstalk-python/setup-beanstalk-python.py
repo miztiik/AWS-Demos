f0626da1a4a7d1626c5fe569ae91935cad6af630
Miztiik <miztiik@users.noreply.github.com>
2017-07-30 18:56:58 +0530

    Update setup-beanstalk-python.py

--- How-To/setup-beanstalk-python/setup-beanstalk-python.py
+++ How-To/setup-beanstalk-python/setup-beanstalk-python.py
@@ -1,7 +1,8 @@
-#!/bin/bash
-# setup-beanstalk-python
-
-
+curl -O https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
+&& yum -y install epel-release-latest-7.noarch.rpm
+yum -y install python-pip
+pip install --upgrade pip
+pip install virtualenv
 
 ## Install Elastic Beanstalk CLI
 pip install --upgrade --user awsebcli
@@ -11,32 +12,32 @@ source ~/.bashrc
 eb --version
 
 
-## Set up your virtual environment
-#### Create a virtual environment named eb-virt:
-virtualenv ~/eb-virt
-#### Activate the virtual environment:
-source ~/eb-virt/bin/activate
-#### Install flask
-pip install flask==0.10.1
 
-### Create Flask Application
-mkdir eb-flask
-cd eb-flask
-touch application.py
-cat >> application.py << "EOF"
+# Create a Virtual Environment for our App
+
+mkdir -p /var/virt-env/elastic-bean-stalk-app
+
+virtualenv /var/virt-env/
+source /var/virt-env/bin/activate
+
+# Install Flask
+pip install flask
+
+cd /var/virt-env/elastic-bean-stalk-app
+
+cat > "/var/virt-env/elastic-bean-stalk-app/application.py" << "EOF"
 from flask import Flask
 
 # print a nice greeting.
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
 
-
 # some bits of text for the page.
 header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+    <html>\n<head> <title>ELastic Bean Stalk Demo<title> </head>\n<body>'''
 instructions = '''
     <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Miztiik</code>) to say hello to
+    to the URL (for example: <code>/Valaxy</code>) to say hello to
     someone specific.</p>\n'''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
@@ -58,19 +59,22 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
-    application.run()
+    application.run(host='0.0.0.0')
 EOF
 
-### Configure your site for Elastic Beanstalk
-source ~/eb-virt/bin/activate
-##### Elastic Beanstalk uses to requirements.txt to determine which package to install on the EC2 instances that run your application.
-pip freeze > requirements.txt
-deactivate
+# Lets Package the app
+source /var/virt-env/bin/activate
 
 ## Deploy Your Site With the EB CLI
 ### Create an environment and deploy your Flask application
-cd ~/eb-virt/eb-flask
-eb init -p python2.7 miztiik-flask-demo
-eb create flask-env
+cd /var/virt-env/elastic-bean-stalk-app
+eb init -p python2.7 prod-env-newsapp
+# Optional Set keypair
+eb init
+
+eb create newsapp
 # For testing in local browser
 eb open
+
+# Clean Up and Next Steps
+eb terminate newsapp
