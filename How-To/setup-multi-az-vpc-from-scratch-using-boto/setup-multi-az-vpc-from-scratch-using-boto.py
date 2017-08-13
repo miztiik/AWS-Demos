23f16dbf07d2485671395991eb3378506d706d4f
Mystique <13oct08@quantumfoam.uni.cc>
2017-08-13 22:22:44 +0530

    Parameterized the vpc, subnet cidr ranges
    Signed-off-by: Mystique <13oct08@quantumfoam.uni.cc>

--- How-To/setup-multi-az-vpc-from-scratch-using-boto/setup-multi-az-vpc-from-scratch-using-boto.py
+++ How-To/setup-multi-az-vpc-from-scratch-using-boto/setup-multi-az-vpc-from-scratch-using-boto.py
@@ -5,28 +5,42 @@
 
 import boto3
 
-REGION_NAME = "ap-south-1"
-AZ1         = "ap-south-1a"
-AZ2         = "ap-south-1b"
-CIDRange    = '10.240.0.0/23'
-tagName     = 'miztiik-vpc-demo-01'
+globalVars  = {}
+globalVars['REGION_NAME']              = "ap-south-1"
+globalVars['AZ1']                      = "ap-south-1a"
+globalVars['AZ2']                      = "ap-south-1b"
+globalVars['CIDRange']                 = "10.240.0.0/23"
+
+globalVars['az1_pvtsubnet_CIDRange']   = "10.240.0.0/25"
+globalVars['az1_pubsubnet_CIDRange']   = "10.240.0.128/26"
+globalVars['az1_sparesubnet_CIDRange'] = "10.240.0.192/26"
+
+globalVars['az2_pvtsubnet_CIDRange']   = "10.240.1.0/25"
+globalVars['az2_pubsubnet_CIDRange']   = "10.240.1.128/26"
+globalVars['az2_sparesubnet_CIDRange'] = "10.240.1.192/26"
+
+globalVars['tagName']                  = "miztiik-vpc-demo-00"
+globalVars['EC2-RH-AMI-ID']            = "ami-cdbdd7a2"
+globalVars['EC2-Amazon-AMI-ID']        = "ami-3c89f653"
+globalVars['EC2-InstanceType']         = "t2.micro"
+globalVars['EC2-KeyName']              = "valaxy-key"
+globalVars['EC2-InstanceType']         = "t2.micro"
+
 
 # Creating a VPC, Subnet, and Gateway
-ec2         = boto3.resource ( 'ec2', region_name = REGION_NAME )
-ec2Client   = boto3.client   ( 'ec2', region_name = REGION_NAME )
-vpc         = ec2.create_vpc ( CidrBlock = CIDRange  )
+ec2         = boto3.resource ( 'ec2', region_name = globalVars['REGION_NAME'] )
+ec2Client   = boto3.client   ( 'ec2', region_name = globalVars['REGION_NAME'] )
+vpc         = ec2.create_vpc ( CidrBlock = globalVars['CIDRange']  )
 
 
 # AZ1 Subnets
-az1_pvtsubnet   = vpc.create_subnet( CidrBlock = '10.240.0.0/25'   , AvailabilityZone = AZ1 )
-az1_pubsubnet   = vpc.create_subnet( CidrBlock = '10.240.0.128/26' , AvailabilityZone = AZ1 )
-az1_sparesubnet = vpc.create_subnet( CidrBlock = '10.240.0.192/26' , AvailabilityZone = AZ1 )
-
-
-# AZ2 Subnets
-az2_pvtsubnet   = vpc.create_subnet( CidrBlock = '10.240.1.0/25'   , AvailabilityZone = AZ2 )
-az2_pubsubnet   = vpc.create_subnet( CidrBlock = '10.240.1.128/26' , AvailabilityZone = AZ2 )
-az2_sparesubnet = vpc.create_subnet( CidrBlock = '10.240.1.192/26' , AvailabilityZone = AZ2 )
+az1_pvtsubnet   = vpc.create_subnet( CidrBlock = globalVars['az1_pvtsubnet_CIDRange'], AvailabilityZone = globalVars['AZ1'] )
+az1_pubsubnet   = vpc.create_subnet( CidrBlock = globalVars['az1_pubsubnet_CIDRange'], AvailabilityZone = globalVars['AZ1'] )
+az1_sparesubnet = vpc.create_subnet( CidrBlock = globalVars['az1_pvtsubnet_CIDRange'], AvailabilityZone = globalVars['AZ1'] )
+# AZ2 Subnet
+az2_pvtsubnet   = vpc.create_subnet( CidrBlock = globalVars['az2_pvtsubnet_CIDRange'], AvailabilityZone = globalVars['AZ2'] )
+az2_pubsubnet   = vpc.create_subnet( CidrBlock = globalVars['az2_pubsubnet_CIDRange'], AvailabilityZone = globalVars['AZ2'] )
+az2_sparesubnet = vpc.create_subnet( CidrBlock = globalVars['az2_pvtsubnet_CIDRange'], AvailabilityZone = globalVars['AZ2'] )
 
 # Enable DNS Hostnames in the VPC
 vpc.modify_attribute( EnableDnsSupport = { 'Value': True } )
@@ -47,15 +61,15 @@ routeTable.associate_with_subnet( SubnetId = az2_pvtsubnet.id )
 intRoute = ec2Client.create_route( RouteTableId = routeTable.id , DestinationCidrBlock = '0.0.0.0/0' , GatewayId = intGateway.id )
 
 # Tag the resources
-tag = vpc.create_tags               ( Tags=[{'Key': tagName , 'Value':'vpc'}] )
-tag = az1_pvtsubnet.create_tags     ( Tags=[{'Key': tagName , 'Value':'az1-private-subnet'}] )
-tag = az1_pubsubnet.create_tags     ( Tags=[{'Key': tagName , 'Value':'az1-public-subnet'}] )
-tag = az1_sparesubnet.create_tags   ( Tags=[{'Key': tagName , 'Value':'az1-spare-subnet'}] )
-tag = az2_pvtsubnet.create_tags     ( Tags=[{'Key': tagName , 'Value':'az2-private-subnet'}] )
-tag = az2_pubsubnet.create_tags     ( Tags=[{'Key': tagName , 'Value':'az2-public-subnet'}] )
-tag = az2_sparesubnet.create_tags   ( Tags=[{'Key': tagName , 'Value':'az2-spare-subnet'}] )
-tag = intGateway.create_tags        ( Tags=[{'Key': tagName , 'Value':'igw'}] )
-tag = routeTable.create_tags        ( Tags=[{'Key': tagName , 'Value':'rtb'}] )
+tag = vpc.create_tags               ( Tags=[{'Key': globalVars['tagName'] , 'Value':'vpc'}] )
+tag = az1_pvtsubnet.create_tags     ( Tags=[{'Key': globalVars['tagName'] , 'Value':'az1-private-subnet'}] )
+tag = az1_pubsubnet.create_tags     ( Tags=[{'Key': globalVars['tagName'] , 'Value':'az1-public-subnet'}] )
+tag = az1_sparesubnet.create_tags   ( Tags=[{'Key': globalVars['tagName'] , 'Value':'az1-spare-subnet'}] )
+tag = az2_pvtsubnet.create_tags     ( Tags=[{'Key': globalVars['tagName'] , 'Value':'az2-private-subnet'}] )
+tag = az2_pubsubnet.create_tags     ( Tags=[{'Key': globalVars['tagName'] , 'Value':'az2-public-subnet'}] )
+tag = az2_sparesubnet.create_tags   ( Tags=[{'Key': globalVars['tagName'] , 'Value':'az2-spare-subnet'}] )
+tag = intGateway.create_tags        ( Tags=[{'Key': globalVars['tagName'] , 'Value':'igw'}] )
+tag = routeTable.create_tags        ( Tags=[{'Key': globalVars['tagName'] , 'Value':'rtb'}] )
 
 # Let create the Public & Private Security Groups
 pubSecGrp = ec2.create_security_group( DryRun = False, 
@@ -69,8 +83,8 @@ pvtSecGrp = ec2.create_security_group( DryRun = False,
                               Description='Private_Security_Group',
                               VpcId= vpc.id
                             )
-pubSecGrp.create_tags(Tags=[{'Key': tagName ,'Value':'public-security-group'}])
-pvtSecGrp.create_tags(Tags=[{'Key': tagName ,'Value':'private-security-group'}])
+pubSecGrp.create_tags(Tags=[{'Key': globalVars['tagName'] ,'Value':'public-security-group'}])
+pvtSecGrp.create_tags(Tags=[{'Key': globalVars['tagName'] ,'Value':'private-security-group'}])
 
 
 # Add a rule that allows inbound SSH, HTTP, HTTPS traffic ( from any source )
